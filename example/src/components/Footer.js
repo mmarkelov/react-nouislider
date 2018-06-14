2c733a683ddd49b298bd122bc7598699890a1f10
mapkeji <mapkeji26@gmail.com>
2018-06-14 14:31:58 +0300

    Move info div to Footer component

--- example/src/components/Footer.js
+++ example/src/components/Footer.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 const Footer = () => (
   <footer>
+    <div className="info">
+      For more info you can check:{' '}
+      <a href="https://refreshless.com/nouislider/">Official site</a>
+    </div>
     Done by <a href="https://github.com/mmarkelov">mmarkelov</a>
   </footer>
 );
