afaeed7b885d6fb78dc5ac9b7ba9bb494fe3f887
mapkeji <mapkeji26@gmail.com>
2018-06-15 11:47:35 +0300

    Add examples

--- example/src/components/Footer.js
+++ example/src/components/Footer.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Footer = () => (
   <footer>
     <div className="info">
+      <h4>More examples coming soon</h4>
       For more info you can check:{' '}
       <a href="https://refreshless.com/nouislider/">Official site</a>
     </div>
