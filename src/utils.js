2db4b6d11df4ef445fbb05039b6b212f23fcf1a6
mapkeji <mapkeji26@gmail.com>
2018-07-18 12:58:13 +0300

    Add utils function

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+export const isEqual = (val1, val2) => {
+  if (typeof val1 === "number" && typeof val2 === "number")
+    return val1 === val2;
+  if (typeof val1 === "string" && typeof val2 === "string")
+    return val1 === val2;
+  if (Array.isArray(val1) && Array.isArray(val2)) {
+    return JSON.stringify(val1) === JSON.stringify(val2);
+  }
+  return false;
+};
