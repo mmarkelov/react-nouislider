dc03d255061ac796d0504556a13e0d840ec33d23
mmarkelov <maks-markel@mail.ru>
2020-11-03 18:15:48 +0300

    Move to Webpack 5

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -8,93 +8,92 @@ const reactDOM = path.resolve('../node_modules/react-dom');
 
 const vendor = ['react', 'react-dom'];
 
-const NODE_ENV = process.env.NODE_ENV || 'development';
+module.exports = (env) => {
+  const isDebug = !env.production;
+  return {
+    devtool: isDebug ? "source-map" : "hidden-source-map",
 
-const isDebug = process.argv[1].indexOf('webpack-dev-server') !== -1;
-
-module.exports = {
-  devtool: NODE_ENV === 'development' ? 'source-map' : '',
-
-  devServer: {
-    open: true,
-    compress: true,
-    port: 3004,
-  },
-
-  entry: {
-    vendor,
-    index: './src/index.js',
-  },
-
-  output: {
-    path: path.join(__dirname, '/build'),
-    publicPath: isDebug ? '/' : '/react-nouislider/',
-    filename: isDebug ? '[name].js' : 'static/js/[name].[chunkhash:8].js',
-  },
-
-  resolve: {
-    alias: {
-      'nouislider-react': reactNoUiSlider,
-      'react': react,
-      'react-dom': reactDOM
+    devServer: {
+      open: true,
+      compress: true,
+      port: 3004,
     },
-  },
 
-  cache: isDebug,
+    entry: {
+      vendor,
+      index: "./src/index.js",
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react'],
-              plugins: [
-                '@babel/plugin-proposal-class-properties',
-                '@babel/plugin-proposal-object-rest-spread',
-              ],
+    output: {
+      path: path.join(__dirname, "/build"),
+      publicPath: isDebug ? "/" : "/react-nouislider/",
+      filename: isDebug ? "[name].js" : "static/js/[name].[chunkhash:8].js",
+    },
+
+    resolve: {
+      alias: {
+        "nouislider-react": reactNoUiSlider,
+        react,
+        "react-dom": reactDOM,
+      },
+    },
+
+    cache: isDebug,
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: "babel-loader",
+              options: {
+                presets: ["@babel/preset-env", "@babel/preset-react"],
+                plugins: [
+                  "@babel/plugin-proposal-class-properties",
+                  "@babel/plugin-proposal-object-rest-spread",
+                ],
+              },
             },
-          },
-        ],
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-            options: { minimize: true },
-          },
-        ],
-      },
-      {
-        test: /\.(gif|png|svg|jpe?g)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'static/assets/[name].[hash:8].[ext]',
+          ],
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+              options: { minimize: true },
             },
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
+          ],
+        },
+        {
+          test: /\.(gif|png|svg|jpe?g)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "static/assets/[name].[hash:8].[ext]",
+              },
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: true,
+        template: "./public/index.html",
+      }),
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      }),
     ],
-  },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: true,
-      template: './public/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-  ],
+  };
 };
